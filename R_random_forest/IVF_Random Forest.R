27074ea127591d807b6dfc7608ee61ddee2778f3
Cartleon <Youngcarleton@gmail.com>
2025-04-25 18:13:14 -0700

    Replacing random forest script to replace

--- R_random_forest/IVF_Random Forest.R
+++ R_random_forest/IVF_Random Forest.R
@@ -2,11 +2,6 @@
 
 # Based on this guide: https://www.r-bloggers.com/2021/04/random-forest-in-r/
 
-
-install.packages("randomForest")
-install.packages("caret")
-
-
 # Load library
 library(randomForest)
 library(caret)
@@ -14,9 +9,8 @@ library(readr)
 library(tidyverse)
 library(dplyr)
 
-#### Make model using the 9 taxa ####
+
 # Load in data that has the ASVs as columns with their counts and the metadata column of interest per sample but remove sample_id
-data <- read.delim("kn_class_filt.csv", sep = ",")
 
 IVF_meta <- read.delim("IVF_metadata_combined.tsv", sep="\t", row.names=1)
 IVF_OTU <- read.delim("feature-table.txt", sep="\t", skip=1, row.names=1)
@@ -25,13 +19,13 @@ t_IVF_OTU <- t(IVF_OTU)
 
 IVF_meta_df <- as.data.frame(IVF_meta)
 IVF_OTU_df <- as.data.frame(t_IVF_OTU)
-
+# Merge metadata and OTU
 IVF_merged <- merge(IVF_OTU_df, IVF_meta_df, by = "row.names", all.x=FALSE)
 
 IVF_filtered <- IVF_merged[, !colnames(IVF_merged) %in% c("Row.names", "AGE", "disease", "age_group", "age_outcome")]
 IVF_filtered <- IVF_filtered[!is.na(IVF_filtered$pregnancy_success), ]
 
-# Ensure sample size is the same across categories ideally
+# Check sample size across pregnancy success
 IVF_filtered$pregnancy_success <- as.factor(IVF_filtered$pregnancy_success)
 table(IVF_filtered$pregnancy_success)
 
@@ -50,16 +44,23 @@ test <- IVF_filtered[ind==2,]
 rf <- randomForest(pregnancy_success ~ ., data = train, proximity=TRUE)
 print(rf)
 
-# you can plot the model
+## Plotting the model
 plot(rf) 
 MDSplot(rf, train$pregnancy_success)
-varImpPlot(rf)
 
-# prediction and confusion matrix
+#Variable Importance Plot
+png("Top5_VariableImportance.png", width = 800, height = 600)
+varImpPlot(rf, n.var = 5, main = "Top 5 - Variable Importance") #Top 5 Variable Importance plot
+dev.off()
+
+#Model Accuracy
+# Prediction and confusion matrix on training data
 p1 <- predict(rf, train)
-confusionMatrix(p1, train$pregnancy_success)
+confusionMatrix(p1, train$ pregnancy_success)
 
-# run prediction and confusion on the test data now
+# Run prediction and confusion on the test data now
 p2 <- predict(rf, test)
-confusionMatrix(p2, test$pregnancy_success)
+confusionMatrix(p2, test$ pregnancy_success)
+
+
 
